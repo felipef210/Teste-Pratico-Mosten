aa31ea34f3d1b43e29f2c0709b33ec4a85f976ba
felipef210 <rfelipe321@live.com>
2025-08-10 12:40:07 -0300

    style: Ajustes no header, footer e modificações nos chips dos cards

--- MoviesMostenAPP/src/app/shared/components/header/header.component.ts
+++ MoviesMostenAPP/src/app/shared/components/header/header.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   menuOpen = false;
+  currentRoute = '';
+
+  constructor(private router: Router) {
+    this.currentRoute = this.router.url;
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
+  }
 }
